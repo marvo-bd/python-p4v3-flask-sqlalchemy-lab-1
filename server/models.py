96bef988ffd85541949cf7b6929180ca75386c84
Marvin Kabira <marvobd@gmail.com>
2024-09-03 00:31:14 +0300

    lab completed

--- server/models.py
+++ server/models.py
@@ -6,4 +6,17 @@ metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'magnitude': self.magnitude,
+            'location': self.location,
+            'year': self.year
+        }
